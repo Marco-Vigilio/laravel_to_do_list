4b096e26cdfb2ca67994b50bf45d78d1b87c9df7
Marco-Vigilio <tonyvigilio@gmail.com>
2024-04-05 10:40:39 +0200

    faccio vedere le task nella view home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +26,76 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Recupera l'utente corrente
+        $user = Auth::user();
+
+        // Recupera la lista dell'utente corrente
+        $lista = $user->lista;
+
+        // Recupera tutte le task associate alla lista dell'utente corrente
+        $tasks = $lista->tasks;
+
+        // Passa le task alla vista
+        return view('home', compact('tasks'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("home");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,49 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    Oggi
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <ul>
+                        @foreach ($tasks as $task)
+                        <li>{{ $task->task }}</li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
+
+            <form action="{{ route('home')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+
+                @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <div class="mb-3">
+                    <label for="exampleFormControlInput" class="form-label">
+                        Title
+                    </label>
+                    <input type="text" class="form-control" id="title" placeholder="Insert your task" name="task" value="{{ old('task', '')}}">
+                </div>
+
+
+
+                <div class="mb-3">
+                    <button type="submit">
+                        Create new task
+                    </button>
+                    <button type="reset">
+                        Reset
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
